4ffbaac86117b493d6a220aca840e537d5bb3db8
xiangjing <xiangjing_cuit@163.com>
2016-03-25 10:07:53 +0800

    [xiangjing]:

--- src/main/java/trello/test_login.feature
+++ src/main/java/trello/test_login.feature
@@ -10,4 +10,17 @@
       那么 登录成功了
       例子:
         | arg0                   | arg1      |
-        | xiangjing_cuit@163.com | Xjay19911 |
+        |xiangjing_cuit@163.com  |Xjay19911|
+
+    场景大纲: 登录失败
+        假设 trello登录页面已经打开
+        当输入用户名"<string>"和密码"<string2>"
+        当点击登录
+        那么登录失败
+        例子:
+          | string | string2 |
+          |        |         |
+          |                      |123         |
+          |2324                  |            |
+          |xiangjing_cuit@163.com|     123    |
+          |123                   |123         |
--- src/main/java/trello/test_login.java
+++ src/main/java/trello/test_login.java
@@ -1,6 +1,5 @@
 package trello;
 
-import cucumber.api.java.Before;
 import cucumber.api.java.zh_cn.假设;
 import cucumber.api.java.zh_cn.当;
 import cucumber.api.java.zh_cn.那么;
@@ -14,16 +13,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class test_login {
     WebDriver dr;
 
-    @Before
-    public void init() {
-        dr = new FirefoxDriver();
-    }
 
     @假设("^trello登录页面已经打开$")
     public void trello登录页面已经打开() throws Throwable {
-
+        dr = new FirefoxDriver();
         dr.get("https://www.trello.com");
-
     }
 
     @当("^输入用户名\"([^\"]*)\"和密码\"([^\"]*)\"$")
@@ -31,17 +25,33 @@ public class test_login {
         dr.findElement(By.className("quiet")).click();
         dr.findElement(By.id("user")).sendKeys(arg0);
         dr.findElement(By.id("password")).sendKeys(arg1);
-
     }
 
     @当("^点击登录$")
     public void 点击登录() throws Throwable {
         dr.findElement(By.id("login")).click();
-      System.out.print("登录成功");
+        Thread.sleep(100000);
     }
 
     @那么("^登录成功了$")
     public void 登录成功了() throws Throwable {
-        System.out.print("登录成功");
+        if(dr.getCurrentUrl().equals("https://trello.com/") ){
+            System.out.println("通过");
+        }
+        else {
+            System.out.println("不通过");
+        }
+        System.out.println(dr.getCurrentUrl());
+    }
+
+    @那么("^登录失败$")
+    public void 登录失败() throws Throwable {
+        if(dr.getCurrentUrl().equals("https://trello.com/login") ){
+            System.out.println("通过");
+        }
+        else {
+            System.out.println("不通过");
+        }
+        System.out.println(dr.getCurrentUrl());
     }
 }
